e60ce96bad39714108248b0da4a19a9e4d11a2bd
ssmpet <ssmpet@gmail.com>
2023-06-16 12:20:24 +0900

    Ch.6 가설검정

--- ch06/02.단일모집단.R
+++ ch06/02.단일모집단.R
@@ -86,3 +86,19 @@ c(upper.limit, p.value)
 
 # R: prop.test
 prop.test(nos, n, p=0.1, alternative='greater', correct=F)
+
+# 도표 출력 : 그림 6-9
+par(mar=c(0,1,1,1))
+x <- seq(-3, 3, by=0.001)
+y <- dnorm(x)
+plot(x, y, type="l", axes=F, ylim=c(-0.02, 0.4), 
+     main="", xlab="z", ylab="")
+abline(h=0)
+
+polygon(c(upper.limit, x[x>upper.limit], 3), c(0, y[x>upper.limit], 0), col=2)
+text(upper.limit, -0.02, expression(z[0.05]==1.645))
+
+polygon(c(z, x[x>z], 3), c(0, y[x>z], 0), density=20, angle=45)
+text(z, -0.02, paste("z=", round(z, 3)))
+text(1.2, 0.3, paste("P(Z>z)=", round(p.value, 3)), cex=0.8)
+
